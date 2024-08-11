9010dd444c4e84dccdaea7c22c8e7eb74361c2b2
Nimisha Rawat <nimisha17rawat@gmail.com>
2024-08-11 18:34:15 +0530

    Improved signup component html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: '/auth/landing', pathMatch: 'full' }, // Redirect to landing page
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: '**', redirectTo: '/auth/landing' } // Redirect unknown paths to landing page
+  { path: '**', redirectTo: '/auth/landing' }, // Redirect unknown paths to landing page
+  
 ];
 
 @NgModule({
